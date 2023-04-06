9e4cde693e5fb8b755e96c7025fd0ec9f8755206
Ahmed Hesham Mostafa <ahmed.hesham.mostafa7@gmail.com>
2023-04-06 21:38:47 +0200

    Update Backbone.php

--- Backbone.php
+++ Backbone.php
@@ -4,22 +4,22 @@
 
 while(1)
 {
-	$lines = file('file.txt');
+	/*$lines = file('file.txt');
 	$line_number = 0;
 	
 
 	if(($lines[0] == 1))
-	{
+	{*/
 		echo($lines[0]);
 		echo("You have a fire!");
-	}
+	/*}
 
 
 	else
-	{
+	{*/
 		echo($lines[0]);
 		echo("No fire!");
-	}
+	/*}*/
 }
 
 
